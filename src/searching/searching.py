428bb0b160b550d0bd48287b5a3c38dad11eb6b1
Ana Zoe <50473626+acarrillo3@users.noreply.github.com>
2020-05-18 20:28:30 -0400

    searching file completo, test are now running and passing

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,6 +1,8 @@
 def linear_search(arr, target):
     # Your code here
-
+    for i in range(len(arr)):
+        if target == arr[i]:
+            return i
 
     return -1   # not found
 
@@ -9,6 +11,27 @@ def linear_search(arr, target):
 def binary_search(arr, target):
 
     # Your code here
+    # let's figure out the total size of the arr
+    left = 0
+    right = len(arr) - 1
 
+    while(left <= right):
+        # find the midpoint
+        mid = (left + right) // 2
+
+        # check to see if hr midpoint element is our target
+        if arr[mid] == target:
+            return mid
+        
+        #check to see if the element should be on the left or right side of our mid point
+        if arr[mid] < target:
+            # get rid of the left side of the arr
+            # update our left index
+            left = mid + 1
+
+        # otherwise, arr[mid] > target
+        else:
+            # get rid of the right side of the arr because the element has to be on the left side
+            right = mid - 1
 
     return -1  # not found
